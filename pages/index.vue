051113e7c8aefe8153378a8d2cc3b5c4fd64fa4a
Jeffrey Lobato <Jeffrey.Lobato@klm.com>
2023-10-22 11:24:41 +0200

    - temp select

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="my-36 container">
+    <CustomSelect :options="options"/>
+    <CustomSelect :options="optionsHover">
+      <template #optionSlot="{option}">
+        <OptionHover :option=option />
+      </template>
+    </CustomSelect>
     <SummarySection/>
     <main>
       <Headline>Views</Headline>
@@ -45,6 +51,25 @@
 import Headline from "~/components/ui/Headline.vue";
 import ViewCard from "~/components/ViewCard.vue";
 import SummarySection from "~/components/SummarySection.vue";
+import OptionHover from "~/components/CustomSelect/OptionHover.vue";
+
+const options = [
+  {label: 'Component Walker', value: 'Component Walker'},
+  {label: 'Component Comparison', value: 'Component Comparison'},
+  {label: 'Component Matrix', value: 'Component Matrix'},
+  {label: 'Component Chord', value: 'Component Chord'},
+  {label: 'Component Plotter', value: 'Component Plotter'},
+  {label: 'Distance to Main Sequence', value: 'Distance to Main Sequence'},
+];
+const optionsHover = [
+  {label: 'Component Walker', value: 'Component Walker', extraInfo: 'Niggers'},
+  {label: 'Component Comparison', value: 'Component Comparison', extraInfo: 'Second'},
+  {label: 'Component Matrix', value: 'Component Matrix', extraInfo: 'Big man flex'},
+  {label: 'Component Chord', value: 'Component Chord', extraInfo: 'Cracker'},
+  {label: 'Component Plotter', value: 'Component Plotter', extraInfo: 'We bombing shit up'},
+  {label: 'Distance to Main Sequence', value: 'Distance to Main Sequence', extraInfo: 'Night night nigga'},
+];
+
 
 
 definePageMeta({
