7054496fd173505b220e00febbd8580b5a8b949a
Ryan Susana <ryansusana97@gmail.com>
2023-11-01 19:09:09 +0100

    give checkbox hover effect

--- components/ui/common/Checkbox.vue
+++ components/ui/common/Checkbox.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <div class="flex items-center justify-center gap-2">
-    <div class="w-4 h-4 cursor-pointer" @click="emit('update:modelValue', !modelValue); emit('click')">
-      <div class="w-full h-full rounded flex justify-center items-center  border-secondary-500 border"
+  <div class="flex items-center justify-center gap-2 group" @click="emit('update:modelValue', !modelValue); emit('click')">
+    <div class="w-4 h-4 cursor-pointer" >
+      <div class="w-full h-full rounded flex justify-center items-center transition-all  border-secondary-500 border group-hover:bg-secondary-50"
       >
         <Icon class="text-secondary-500 transition-all duration-150 ease-in-out" :class="{'opacity-0': !modelValue, 'scale-0': !modelValue}" :size="24" icon="check"></Icon>
       </div>
